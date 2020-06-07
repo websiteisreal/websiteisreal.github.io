500fbdef4108407328279e1a470d0892f6261310
websiteisreal <38820519+websiteisreal@users.noreply.github.com>
2020-06-07 17:12:02 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The Face Mask Company</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <script src="js/scripts.js"></script>
+  <h1> Welcome to the Face Mask Company Website</h1>
+</body>
+</html>
